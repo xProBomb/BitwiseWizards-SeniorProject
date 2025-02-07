929e578de73bd7680c5e6c4d213073355ecdec48
xProBomb <Justbomb03@gmail.com>
2025-02-07 14:00:03 -0800

    Hypothetically Added DB
    Followed the Tutorial from 460 and I think got it working

---
+++ TrustTrade/TrustTrade/Models/Like.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrustTrade.Models;
+
+public partial class Like
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("UserID")]
+    public int UserId { get; set; }
+
+    [Column("PostID")]
+    public int PostId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedAt { get; set; }
+
+    [ForeignKey("PostId")]
+    [InverseProperty("Likes")]
+    public virtual Post Post { get; set; } = null!;
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Likes")]
+    public virtual User User { get; set; } = null!;
+}
