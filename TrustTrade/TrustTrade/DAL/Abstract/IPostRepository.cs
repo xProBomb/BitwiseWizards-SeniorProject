dbe6e392c98e980289294cf4941952698028c6ba
Jesus-Larios <jesuslarios1508@gmail.com>
2025-02-10 19:56:01 -0800

    Added method to retrive posts by pagination

---
+++ TrustTrade/TrustTrade/DAL/Abstract/IPostRepository.cs
@@ -0,0 +1,9 @@
+using TrustTrade.Models;
+
+namespace TrustTrade.DAL.Abstract;
+
+public interface IPostRepository : IRepository<Post>
+{
+    List<Post> GetPagedPosts(int page = 1, int pageSize = 10);
+
+}
