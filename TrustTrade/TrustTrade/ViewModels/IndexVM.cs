e1f73ce43b97fc19d87df954ae1876b27511aba6
Jesus-Larios <jesuslarios1508@gmail.com>
2025-02-14 14:21:37 -0800

    Added field a list of pages to show for pagination

--- TrustTrade/TrustTrade/ViewModels/IndexVM.cs
+++ TrustTrade/TrustTrade/ViewModels/IndexVM.cs
@@ -7,4 +7,5 @@ public class IndexVM
     public List<PostPreviewVM>? Posts { get; set; }
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
+    public List<int>? PagesToShow { get; set; }
 }
