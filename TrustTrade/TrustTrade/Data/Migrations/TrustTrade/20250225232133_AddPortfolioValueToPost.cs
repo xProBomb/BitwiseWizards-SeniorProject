375eee5a3fe4b511d1e6c2ce2532717dd4cb7b98
Palwinder Sangha <Povensangha@ymail.com>
2025-02-25 21:26:07 -0800

    Added a verification ticker to posts, still working on tests

---
+++ TrustTrade/TrustTrade/Data/Migrations/TrustTrade/20250225232133_AddPortfolioValueToPost.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrustTrade.Data.Migrations.TrustTrade
+{
+    /// <inheritdoc />
+    public partial class AddPortfolioValueToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PortfolioValueAtPosting",
+                table: "Posts",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PortfolioValueAtPosting",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
