4c039b167e9ae406ba48124f1e596e7705e144e2
Jesus-Larios <jesuslarios1508@gmail.com>
2025-02-28 15:54:07 -0800

    Added not null to TagName column in Tags table

--- TrustTrade/TrustTrade/Models/Tag.cs
+++ TrustTrade/TrustTrade/Models/Tag.cs
@@ -7,7 +7,7 @@ public partial class Tag
 {
     public int Id { get; set; }
 
-    public string? TagName { get; set; }
+    public string TagName { get; set; } = null!;
 
     public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
 }
