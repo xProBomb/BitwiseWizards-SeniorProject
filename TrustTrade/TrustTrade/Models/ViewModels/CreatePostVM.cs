7f1bd5e8d2efee12238e622c4053a4c0bfaf34a2
Jesus-Larios <jesuslarios1508@gmail.com>
2025-03-04 15:41:22 -0800

    Removed field for new tags

--- TrustTrade/TrustTrade/Models/ViewModels/CreatePostVM.cs
+++ TrustTrade/TrustTrade/Models/ViewModels/CreatePostVM.cs
@@ -5,14 +5,12 @@ namespace TrustTrade.ViewModels;
 public class CreatePostVM
 {
     [Required]
-    public required string Title { get; set; }
+    public string? Title { get; set; }
 
     [Required]
-    public required string Content { get; set; }
+    public string? Content { get; set; }
 
     public List<string> ExistingTags { get; set; } = new List<string>();
 
     public List<string> SelectedTags { get; set; } = new List<string>();
-
-    public List<string> NewTags { get; set; } = new List<string>();
 }
