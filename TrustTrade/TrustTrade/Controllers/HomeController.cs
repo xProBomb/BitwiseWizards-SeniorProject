2665f02c50b29bbddb6be9d72c03c03d1f53830f
Jesus-Larios <jesuslarios1508@gmail.com>
2025-03-07 20:08:10 -0800

    Moved post creation from HomeController to PostsController

--- TrustTrade/TrustTrade/Controllers/HomeController.cs
+++ TrustTrade/TrustTrade/Controllers/HomeController.cs
@@ -4,31 +4,23 @@ using TrustTrade.Models;
 using TrustTrade.DAL.Abstract;
 using TrustTrade.ViewModels;
 using TrustTrade.Helpers;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 
 namespace TrustTrade.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<IdentityUser> _userManager;
         private readonly IPostRepository _postRepository;
         private readonly ITagRepository _tagRepository;
-        private readonly IUserRepository _userRepository;
 
         public HomeController(
             ILogger<HomeController> logger, 
-            UserManager<IdentityUser> userManager, 
             IPostRepository postRepository, 
-            ITagRepository tagRepository, 
-            IUserRepository userRepository)
+            ITagRepository tagRepository)
         {
             _logger = logger;
-            _userManager = userManager;
             _postRepository = postRepository;
             _tagRepository = tagRepository;
-            _userRepository = userRepository;
         }
 
         public IActionResult Index(string? categoryFilter = null, int page = 1, string sortOrder = "DateDesc")
@@ -78,68 +70,6 @@ namespace TrustTrade.Controllers
             };
 
             return View(vm);
-        }
-
-        [Authorize]
-        [HttpGet]
-        public IActionResult CreatePost()
-        {
-            // Retrieve all existing tags for the view model
-            CreatePostVM vm = new CreatePostVM
-            {
-                ExistingTags = _tagRepository.GetAllTagNames()
-            };
-
-            return View(vm);
-        }
-
-        [Authorize]        
-        [HttpPost]
-        public IActionResult CreatePost(CreatePostVM createPostVM)
-        {
-            if (ModelState.IsValid)
-            {
-                string? identityUserId = _userManager.GetUserId(User);
-                if (identityUserId == null)
-                {
-                    return Unauthorized();
-                }
-
-                // Retrieve the user from the database
-                User? user = _userRepository.FindByIdentityId(identityUserId);
-                if (user == null)
-                {
-                    return NotFound();
-                }
-
-                // Map the CreatePostVM to the Post entity
-                Post post = new Post
-                {
-                    UserId = user.Id,
-                    Title = createPostVM.Title,
-                    Content = createPostVM.Content
-                };
-
-                // Add the selected tags to the post
-                foreach (string tagName in createPostVM.SelectedTags)
-                {
-                    Tag? tag = _tagRepository.GetTagByName(tagName);
-                    if (tag != null)
-                    {
-                        // Add the tag to the post and the post to the tag
-                        post.Tags.Add(tag);
-                        tag.Posts.Add(post);
-                    }
-                }
-
-                // Save the post to the database
-                _postRepository.AddOrUpdate(post);
-                return RedirectToAction("Index");
-            }
-
-            // Repopulate the existing tags in case of validation errors
-            createPostVM.ExistingTags = _tagRepository.GetAllTagNames();
-            return View(createPostVM);
         }
 
         public IActionResult Privacy()
