f0436109a094c34540fc555a438ed588f741ee25
Jesus-Larios <jesuslarios1508@gmail.com>
2025-03-11 16:45:59 -0700

    Added minimum length requirement for title and content

--- TrustTrade/TrustTrade/Models/ViewModels/CreatePostVM.cs
+++ TrustTrade/TrustTrade/Models/ViewModels/CreatePostVM.cs
@@ -4,15 +4,15 @@ namespace TrustTrade.ViewModels;
 
 public class CreatePostVM
 {
-    [Required]
+    [Required(ErrorMessage = "Title is required.")]
+    [MinLength(5, ErrorMessage = "Title must be at least 5 characters long.")]
     public string? Title { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Content is required.")]
+    [MinLength(10, ErrorMessage = "Content must be at least 10 characters long.")]
     public string? Content { get; set; }
 
     public List<string> ExistingTags { get; set; } = new List<string>();
 
     public List<string> SelectedTags { get; set; } = new List<string>();
-    
-    public decimal? PortfolioValueAtPosting { get; set; }
 }
