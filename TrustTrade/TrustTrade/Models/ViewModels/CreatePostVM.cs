17904c25fa7e38c0ee02cfb35e34b53151ea091f
Jesus-Larios <jesuslarios1508@gmail.com>
2025-03-12 21:17:44 -0700

    Removed comment that was no longer needed

--- TrustTrade/TrustTrade/Models/ViewModels/CreatePostVM.cs
+++ TrustTrade/TrustTrade/Models/ViewModels/CreatePostVM.cs
@@ -14,15 +14,14 @@ public class CreatePostVM
     [StringLength(1024, MinimumLength = 10, ErrorMessage = "Content must be between 10 and 1024 characters long.")]
     public string? Content { get; set; }
 
+    [Display(Name = "Privacy Setting")]
+    [Required(ErrorMessage = "Privacy setting is required.")]
+    public bool? IsPublic { get; set; }
+
     // Tag properties
     [Display(Name = "Tags")]
     public List<string> ExistingTags { get; set; } = new List<string>();
 
     [MaxLength(5, ErrorMessage = "No more than 5 tags are allowed.")]
     public List<string> SelectedTags { get; set; } = new List<string>();
-
-    // Privacy properties
-    [Display(Name = "Privacy Setting")]
-    [Required(ErrorMessage = "Privacy setting is required.")]
-    public bool? IsPublic { get; set; }
 }
