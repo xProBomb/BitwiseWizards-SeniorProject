597055342e82c84623a94b10869c3f29ed369351
Jesus-Larios <jesuslarios1508@gmail.com>
2025-03-14 13:15:07 -0700

    Fixed current time to use local time instead of utc

--- TrustTrade/TrustTrade/Helpers/TimeAgoHelper.cs
+++ TrustTrade/TrustTrade/Helpers/TimeAgoHelper.cs
@@ -4,7 +4,7 @@ public static class TimeAgoHelper
 {
     public static string GetTimeAgo(DateTime? pastDate, DateTime? currentDate = null)
     {
-        DateTime now = currentDate ?? DateTime.UtcNow; // If currentDate is not provided, use DateTime.Now
+        DateTime now = currentDate ?? DateTime.UtcNow.ToLocalTime(); // If currentDate is not provided, use DateTime.Now
         if (pastDate == null)
         {
             return "Date not available";
