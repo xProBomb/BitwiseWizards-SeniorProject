9f9ec022426e40d9609b1e857ee0d6f69f985214
Jesus-Larios <jesuslarios1508@gmail.com>
2025-04-14 11:24:09 -0700

    Modified PagedPosts methods to have default values for parameters

--- TrustTrade/TrustTrade/DAL/Abstract/IPostRepository.cs
+++ TrustTrade/TrustTrade/DAL/Abstract/IPostRepository.cs
@@ -26,7 +26,7 @@ public interface IPostRepository : IRepository<Post>
     /// <param name="pageSize">Number of posts per page.</param>
     /// <param name="sortOrder">Sorting order (e.g., "DateDesc", "DateAsc").</param>
     /// <returns>A list of posts that the user is following.</returns>
-    Task<List<Post>> GetPagedPostsByUserFollowsAsync(int currentUserId, string? categoryFilter, int pageNumber, int pageSize, string sortOrder);
+    Task<List<Post>> GetPagedPostsByUserFollowsAsync(int currentUserId, string? categoryFilter = null, int pageNumber = 1, int pageSize = 10, string sortOrder = "DateDesc");
 
     /// <summary>
     /// Get a paginated list of posts by a specific user, optionally filtered by category and sorted by specified order.
@@ -37,7 +37,7 @@ public interface IPostRepository : IRepository<Post>
     /// <param name="pageSize">Number of posts per page.</param>
     /// <param name="sortOrder">Sorting order (e.g., "DateDesc", "DateAsc").</param>
     /// <returns>A list of posts by the specified user.</returns>
-    Task<List<Post>> GetPagedPostsByUserAsync(int userId, string? categoryFilter, int pageNumber, int pageSize, string sortOrder);
+    Task<List<Post>> GetPagedPostsByUserAsync(int userId, string? categoryFilter = null, int pageNumber = 1, int pageSize = 10, string sortOrder = "DateDesc");
 
     /// <summary>
     /// Get the total number of posts, optionally filtered by category.
@@ -52,7 +52,7 @@ public interface IPostRepository : IRepository<Post>
     /// <param name="currentUserId">The ID of the current user.</param>
     /// <param name="categoryFilter">Optional category filter.</param>
     /// <returns>The total number of posts that the user is following.</returns>
-    Task<int> GetTotalPostsByUserFollowsAsync(int currentUserId, string? categoryFilter);
+    Task<int> GetTotalPostsByUserFollowsAsync(int currentUserId, string? categoryFilter = null);
 
     /// <summary>
     /// Get the total number of posts by a specific user, optionally filtered by category.
