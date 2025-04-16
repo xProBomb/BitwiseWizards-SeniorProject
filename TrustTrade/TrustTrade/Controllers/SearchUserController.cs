9f42671363433e72ebf842133c1df56720e16a92
Jesus-Larios <jesuslarios1508@gmail.com>
2025-04-15 18:33:35 -0700

    Added ability to search for posts

--- TrustTrade/TrustTrade/Controllers/SearchUserController.cs
+++ TrustTrade/TrustTrade/Controllers/SearchUserController.cs
@@ -3,16 +3,22 @@ using System.Threading.Tasks;
 using TrustTrade.Data;
 using TrustTrade.Models;
 using System.Collections.Generic;
+using TrustTrade.Services.Web.Interfaces;
+using TrustTrade.ViewModels;
 
 namespace TrustTrade.Controllers
 {
     public class SearchController : Controller
     {
         private readonly ISearchUserRepository _searchuserRepository;
+        private readonly IPostService _postService;
 
-        public SearchController(ISearchUserRepository searchuserRepository)
+        public SearchController(
+            ISearchUserRepository searchuserRepository, 
+            IPostService postService)
         {
             _searchuserRepository = searchuserRepository;
+            _postService = postService;
         }
 
         // Renders the main Search page
@@ -20,6 +26,31 @@ namespace TrustTrade.Controllers
         public IActionResult SearchUser()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchPosts(string search, string? categoryFilter = null, int pageNumber = 1, string sortOrder = "DateDesc")
+        {
+            if (string.IsNullOrWhiteSpace(search))
+            {
+                return RedirectToAction("SearchUser");
+            }
+
+            List<string> searchTerms = search.Split(' ').ToList();
+            List<PostPreviewVM> postPreviews = await _postService.SearchPostsAsync(searchTerms, categoryFilter, pageNumber, sortOrder);
+
+            PostFiltersPartialVM postFiltersVM = await _postService.BuildPostFiltersAsync(categoryFilter, sortOrder, search);
+            PaginationPartialVM paginationVM = await _postService.BuildSearchPaginationAsync(search, searchTerms, categoryFilter, pageNumber);
+
+            var vm = new SearchPostResultsVM
+            {
+                Posts = postPreviews,
+                Pagination = paginationVM,
+                PostFilters = postFiltersVM,
+                SearchQuery = search,
+            };
+
+            return View("SearchPostResults", vm);
         }
 
         // This action is called asynchronously to search for users in real time
