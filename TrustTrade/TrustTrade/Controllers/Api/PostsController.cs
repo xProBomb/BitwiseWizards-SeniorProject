b8f97443f4316b40b9c7f50cbea12472fd87aecb
Jesus-Larios <jesuslarios1508@gmail.com>
2025-04-26 19:44:39 -0700

    Added ability to create comment on a post

---
+++ TrustTrade/TrustTrade/Controllers/Api/PostsController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TrustTrade.Models;
+using TrustTrade.Services.Web.Interfaces;
+using TrustTrade.Models.DTO;
+using TrustTrade.Helpers;
+using TrustTrade.Models.ExtensionMethods;
+
+namespace TrustTrade.Controllers.Api
+{
+    public class PostsController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+        private readonly IUserService _userService;
+        private readonly ILogger<PostsController> _logger;
+
+        public PostsController(
+            ICommentService commentService, 
+            IUserService userService, 
+            ILogger<PostsController> logger)
+        {
+            _commentService = commentService;
+            _userService = userService;
+            _logger = logger;
+        }
+
+        // GET: api/posts/{postId}/comments/{commentId}
+        [HttpGet("{postId}/comments/{commentId}")]
+        public async Task<IActionResult> GetComment(int postId, int commentId)
+        {
+            // TODO: Implement fetching the comment by postId and commentId if needed in the future
+            return Ok(); // Temporary response so the API doesn't break
+        }
+
+        // POST: api/posts/{postId}/comments
+        [HttpPost("{postId}/comments")]
+        [Authorize]
+        public async Task<IActionResult> CreateComment(int postId, [FromBody] CommentCreateDTO commentCreateDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                User? user = await _userService.GetCurrentUserAsync(User);
+                if (user == null) return Unauthorized();
+
+                Comment comment = await _commentService.CreateCommentAsync(postId, user, commentCreateDTO);
+
+                var responseDTO = comment.ToResponseDTO();
+
+                return CreatedAtAction(nameof(GetComment), new { postId, commentId = comment.Id }, responseDTO);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating comment for post {PostId}", postId);
+                return StatusCode(500, "An error occurred while creating the comment.");
+            }
+        }
+    }
+}
--- TrustTrade/TrustTrade/Services/Web/Interfaces/ICommentService.cs
+++ TrustTrade/TrustTrade/Services/Web/Interfaces/ICommentService.cs
@@ -1,6 +1,5 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using TrustTrade.Models;
+using TrustTrade.Models.DTO;
 using TrustTrade.ViewModels;
 
 namespace TrustTrade.Services.Web.Interfaces;
@@ -16,4 +15,13 @@ public interface ICommentService
     /// <param name="postId">The ID of the post.</param>
     /// <returns>A list of comments for the specified post.</returns>
     Task<List<CommentVM>> GetPostCommentsAsync(int postId);
+
+    /// <summary>
+    /// Creates a new comment for a specific post.
+    /// </summary>
+    /// <param name="postId">The ID of the post.</param>
+    /// <param name="user">The user creating the comment.</param>
+    /// <param name="commentDTO">The comment data transfer object.</param>
+    /// <returns>The created comment.</returns>
+    Task<Comment> CreateCommentAsync(int postId, User user, CommentCreateDTO commentCreateDTO);
 }
