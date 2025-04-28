1c26c460aaf3072cd44314120e726de77b8b991e
Jesus-Larios <jesuslarios1508@gmail.com>
2025-04-27 19:06:44 -0700

    Removed unused DTO comment response

--- TrustTrade/TrustTrade/Models/ExtensionMethods/CommentExtensions.cs
+++ TrustTrade/TrustTrade/Models/ExtensionMethods/CommentExtensions.cs
@@ -9,29 +9,6 @@ namespace TrustTrade.Models.ExtensionMethods
     /// </summary>
     public static class CommentExtensions
     {
-        public static CommentResponseDTO ToResponseDTO(this Comment comment)
-        {
-            string? portfolioValue = null;
-
-            if (comment.User.PlaidEnabled == true)
-            {
-                portfolioValue = comment.PortfolioValueAtPosting.HasValue
-                    ? FormatCurrencyAbbreviate.FormatCurrencyAbbreviated(comment.PortfolioValueAtPosting.Value)
-                    : "$0";
-            }
-
-            return new CommentResponseDTO
-            {
-                Id = comment.Id,
-                Username = comment.User.Username ?? string.Empty,
-                Content = comment.Content,
-                TimeAgo = TimeAgoHelper.GetTimeAgo(comment.CreatedAt),
-                IsPlaidEnabled = comment.User.PlaidEnabled ?? false,
-                PortfolioValueAtPosting = portfolioValue,
-                ProfilePicture = comment.User.ProfilePicture,
-            };
-        }
-
         public static CommentVM ToViewModel(this Comment comment)
         {
             string? portfolioValue = null;
