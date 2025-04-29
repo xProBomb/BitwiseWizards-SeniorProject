835f7f3dcf982843ac180a876fa257ed4657f288
Rowan Vasen <rowvasen101@gmail.com>
2025-04-28 21:55:10 -0700

    added in repository for admin features

---
+++ TrustTrade/TrustTrade/DAL/Concrete/AdminRepository.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using TrustTrade.Models;
+
+public class AdminRepository : IAdkijRepository
+{
+    private readonly TrustTradeDbContext _context;
+    public AdminRepository(TrustTradeDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByUsernameAsync(string username)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Username == username);
+    }
+
+    public async Task<List<User>> GetAllAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<List<User>> SearchByUsernameAsync(string searchTerm)
+    {
+        return await _context.Users
+            .Where(u => u.Username.Contains(searchTerm))
+            .ToListAsync();
+    }
+
+    public async Task<User?> FindByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+}
