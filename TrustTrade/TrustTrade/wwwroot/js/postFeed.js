2d0f6cec24c1560cae5f24a67fd0052f2c1503da
Jesus-Larios <jesuslarios1508@gmail.com>
2025-05-21 13:34:29 -0700

    Updated to load more posts using infinite scroll

--- TrustTrade/TrustTrade/wwwroot/js/postFeed.js
+++ TrustTrade/TrustTrade/wwwroot/js/postFeed.js
@@ -1,8 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Make posts clickable to redirect to their respective pages
-    document.querySelectorAll(".clickable-post").forEach(post => {
-        post.addEventListener("click", () => {
+    document.getElementById("postFeedContainer").addEventListener("click", function (e) {
+        const post = e.target.closest(".clickable-post");
+        if (post) {
             window.location.href = post.dataset.url;
-        });
+        }
     });
+
+
+    let currentPage = 2; // Start from page 2 since page 1 is already loaded
+    let isLoading = false;
+
+    // Set up infinite scroll for loading more posts
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            loadMorePosts();
+        }
+    }, {
+        rootMargin: '200px'
+    });
+
+    observer.observe(document.getElementById('loadTrigger'));
+
+    async function loadMorePosts() {
+        if (isLoading) return;
+
+        isLoading = true;
+        document.getElementById('loading').style.display = 'block';
+        category = document.getElementById('categoryFilter').value;
+
+        const response = await fetch(`/posts/loadmore?pageNumber=${currentPage}&categoryFilter=${category}`)
+
+        if (!response.ok) {
+            console.error('Failed to load more posts');
+            return;
+        }
+
+        const postsFeedHtml = await response.text();
+        
+        if (postsFeedHtml.trim() === "") {
+            document.getElementById('endMessage').style.display = 'block';
+            document.getElementById('loading').style.display = 'none';
+            observer.disconnect();
+            return;
+        }
+
+        document.getElementById('postFeedContainer').insertAdjacentHTML('beforeend', postsFeedHtml);
+
+        currentPage++;
+        isLoading = false;
+        console.log('Loaded more posts:', currentPage);
+        document.getElementById('loading').style.display = 'none';
+    }
 });
